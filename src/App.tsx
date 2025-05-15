d3bc65ba91ae0617882b74a2643c78d7dca2fcc9
Lucas Monteiro <lucas.monteiro58@gmail.com>
2025-05-15 12:03:24 -0300

    feat: ✨ add header and summary components with styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { ThemeProvider } from "styled-components";
-import { defaultTheme } from "./styles/themes/default";
+import { Transactions } from "./pages/Transactions";
 import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-      <h1>Hello Word</h1>)
+      <Transactions />
     </ThemeProvider>
   );
 }
